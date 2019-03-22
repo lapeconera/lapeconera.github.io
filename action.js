7353b91b535d2e7da22b64722eeb5a37f25181f3
Dodam <lapeconera@gmail.com>
2019-03-22 19:13:32 +1100

    initial commit

---
+++ action.js
@@ -0,0 +1,15 @@
+// $('#icon1').click(function(){
+//     document.getElementById('img1').style.width = "100px";
+//     document.getElementById('img1').style.display = "block";
+// });
+// $('#img1').click(function close1(){
+//     document.getElementById('img1').style.display = "none";
+// });
+
+function openNav1(val) {
+    document.getElementById(val).style.display = "block";
+}
+
+function closeNav1(val) {
+document.getElementById(val).style.display = "none";
+}
